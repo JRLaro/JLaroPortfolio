7389e8bff2717d7721c276ca48a6e7a839108627
J.Laro <joselarojr@gmail.com>
2021-04-23 10:29:22 -0400

    removed unused link tags

--- src/pages/TestingHomePage.js
+++ src/pages/TestingHomePage.js
@@ -1,42 +1,56 @@
 import React, { useEffect, useState, useRef } from "react";
 import * as faIcons from "react-icons/fa";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Logo from "../images/jLaroBlack.png";
 import Parallax from "parallax-js";
 
-
-
-
 function TestingHomePage() {
   useEffect(() => {
-    var scene = document.getElementById('scene');
-  var parallaxInstance = new Parallax(scene);
-  }, [])
+    var scene = document.getElementById("scene");
+    var parallaxInstance = new Parallax(scene);
+  }, []);
 
   return (
     <div className="HomePage">
       <header className="hero">
-        
         <div id="scene">
-          <img data-depth=".5" src={Logo} alt="j.Laro logo" style={{ width: "30rem", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+          <img
+            data-depth=".5"
+            src={Logo}
+            alt="j.Laro logo"
+            style={{
+              width: "30rem",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
         </div>
-        
+
         <h1 className="hero-text">
           Hi, I am
-           <Link to='/about' style={{textDecoration:"none"}}>
-          <span> Jose Laro </span>
-           </Link>
-        </h1>
-        <p className="h-sub-text">
-          Web Developer | Graphic Designer
-        </p>
-        <div className="icons">
-          <Link to="https://www.linkedin.com/in/jlaro/" target="_blank" className="icon-holder">
-            <faIcons.FaLinkedin className="icon ln" />
-            </Link>
-          <Link to="https://github.com/JRLaro" target="_blank" className="icon-holder">
-            <faIcons.FaGithub className="icon gh" />
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <span> Jose Laro </span>
           </Link>
+        </h1>
+        <p className="h-sub-text">Web Developer | Graphic Designer</p>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/jlaro/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-holder"
+          >
+            <faIcons.FaLinkedin className="icon ln" />
+          </a>
+          <a
+            href="https://github.com/JRLaro"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-holder"
+          >
+            <faIcons.FaGithub className="icon gh" />
+          </a>
         </div>
       </header>
     </div>
